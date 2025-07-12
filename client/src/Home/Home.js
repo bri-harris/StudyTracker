998a91a86eab896e30795dd439a470634aab1332
bri-harris <bri.har20@gmail.com>
2025-07-12 18:40:33 -0400

    backend server sending json to front end, we can see it, this is for testing

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,9 +1,22 @@
+import React, { useState, useRef, useEffect } from 'react';
 import "./Home.css";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
-    
+
+    // create a state variable that will contain the backend data retreived from backend API
+    const [backendData, setBackendData] = useState([{}])
+    useEffect(() => {
+        fetch("/api").then(
+            response => response.json()
+        ).then(
+            data => {
+                setBackendData(data)
+            }
+        )
+    }, [])
+
     return (
 
         <div className="home-page-container">
@@ -12,13 +25,21 @@ const Home = () => {
             <div class="description">
                 <div class="desc-header">Study Tracker</div>
                 <div class="desc-text">The Study Tracker application leverages scheduling, prioritization, and organization with the proven Pomodoro study method to maximize the efficacy of your study time.</div>
+                <div class="desc-text">
+                    {(typeof backendData.users === 'undefined') ? (<p>Loading...</p>)
+                        : (
+                            backendData.users.map((user, i) => (
+                                <p key={i}>{user}</p>
+                            ))
+                        )}
+                </div>
             </div>
 
             <div class="home-picture">
-                <img src="main_pomodoro.png" alt="bolCover" class="" id="heroImage"/>
+                <img src="main_pomodoro.png" alt="bolCover" class="" id="heroImage" />
             </div>
 
-            
+
 
             <Footer />
 
