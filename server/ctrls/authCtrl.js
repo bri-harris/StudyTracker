f99db332de676bb8bbe44232c0067a7910443990
bri-harris <bri.har20@gmail.com>
2025-07-12 22:23:31 -0400

    implemented user registration, authentication and authorization, as well as refresh and logout flows with JWT validation

--- server/ctrls/authCtrl.js
+++ server/ctrls/authCtrl.js
@@ -2,7 +2,12 @@ const usersDB = {
     users: require('../model/users.json'),
     setUsers: function (data) { this.users = data }
 }
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); //user password authenitcation
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
@@ -12,8 +17,30 @@ const handleLogin = async (req, res) => {
     //evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-        //this is where we would create and send a JWT (normal and refresh token)
-        res.json({ 'success': `User ${user} is logged in!` });
+        //create access & refresh token here
+        const accessToken = jwt.sign(
+            //pass in a payload, well use username
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '100s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        //save the refresh token with the current user in the database, this allows a logout route in the future to invalidate the token at logout
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+        //send refresh token as http cookie
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        // res.cookie('jwt', refreshToken, {httpOnly: true, sameSite:'None', secure: true, maxAge: 24 * 60 * 60 * 1000});
+        res.json({ accessToken }); //send access token to user
     } else {
         res.sendStatus(401);
     }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const validateJWTToken = require('./middleware/validateJWTToken');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.port || 5000;
 
 //CORS: cross origin resource sharing
@@ -15,6 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 //built-in middleware for json, supplied to all routes as it comes in
 app.use(express.json());
 
+//middleware for cookies
+app.use(cookieParser())
+
 //testing, was able to get this onto the react front
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] })
@@ -23,6 +28,11 @@ app.get("/api", (req, res) => {
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh')); //refresh token might not be needed
+app.use('/logout', require('./routes/logout')); //refresh token might not be needed
+
+//need a valid session and JWT Token for all routes below
+app.use(validateJWTToken)
 app.use('/students', require('./routes/api/students'))
 
 //backend server running on port 5000, client server (REACT) will be running on port 3000
