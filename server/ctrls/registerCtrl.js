cf1412b8e479b2b19fdb2b9e5419aceaf9282f3f
bri-harris <bri.har20@gmail.com>
2025-07-13 00:00:25 -0400

    User Data Model created, APIs updated, Connected to DB

--- server/ctrls/registerCtrl.js
+++ server/ctrls/registerCtrl.js
@@ -1,34 +1,27 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 //handler for the new user information we receive at this register route
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
     //check for duplicate usernames in our database
-    const duplicate = usersDB.users.find(person => person.username === user);
+    const duplicate = await User.findOne({ username: user }).exec();
     if (duplicate) return res.sendStatus(409); //Conflict
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        const newUser = {
-            "username": user,
-            "roles": { "User": 2222 }, //default role of User       }
-            "password": hashedPwd
-        }
-        usersDB.setUsers([...usersDB.users, newUser]);
 
-        //write to our json file which is our db in this example
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
+        //able to create and store the new user with mongoose
+        //regular role 'user' is assigned by default in the User model
+        const result = await User.create({
+            "username": user,
+            "password": hashedPwd
+        })
+
+        console.log(result);//record thats been created
+
         res.status(201).json({ 'success': `New user ${user} created!` });
     } catch (err) {
         res.status(500).json({ 'message': err.message });
